2d0202e62bcb7d54f47209cb27a4d41679d01c99
technology <technology@CPX-B0CGZADBE1E.dir.svc.accenture.com>
2019-05-21 20:29:06 -0500

    Base del proyecto

---
+++ EjercicioLunes2/EjercicioLunes2/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjercicioLunes2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int contador;
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Operaciones resultado = new Operaciones();
+
+            label1.Text = resultado.Sumar().ToString();
+
+        }
+        
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = contador++.ToString();
+        }
+    }
+}
---
+++ EjercicioLunes2/EjercicioLunes2/Operaciones.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioLunes2
+{
+    class Operaciones
+    {
+        public long Sumar() {
+
+            long val = 0;
+
+            for (long i=0; i<1000000;i++)
+            {
+                val += i;
+            }
+            return val;
+        }
+    }
+}
